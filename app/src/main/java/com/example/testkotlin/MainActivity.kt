19b8d4099ca50bf1432b3d4b5142a85d8a1a302f
wangkang <wk@8687@closeli.cn>
2019-11-21 17:06:21 +0800

    1

--- app/src/main/java/com/example/testkotlin/MainActivity.kt
+++ app/src/main/java/com/example/testkotlin/MainActivity.kt
@@ -2,11 +2,49 @@ package com.example.testkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    var str: String = "df"
+
+    var datas :List<Int>?=null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        str.first()
+        str.last()
+        datas = listOf(1,3,5,23)
+
+        var result = datas!!.all {
+            it%2==1
+        }
+        var result2 = datas!!.map {   it<15  }
+        //result.toString()
+
+        tv.setText(  result2.joinToString (prefix = "{",postfix = "}"))
+        tv.setOnClickListener {
+            System.out.println("ddf")
+        }
+        Xiecheng()
+    }
+
+
+    fun Xiecheng() {
+        GlobalScope.launch { // launch a new coroutine in background and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            //println("World!") // print after delay
+            Log.d("TAG","World!")
+        }
+        Log.d("TAG","Hello,")
+        //println("Hello,") // main thread continues while coroutine is delayed
+        Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
     }
 }
